2a90e0cc3ae1d4c8b6649c2a17dbaf68282e3dcc
fernando-castelo <nando.castelobranco24@hotmail.com>
2023-11-15 18:41:09 -0300

    login do tutor

---
+++ src/java/com/devcaotics/controllers/LoginController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devcaotics.controllers;
+
+import com.devcaotics.model.Tutor;
+import com.devcaotics.model.dao.ManagerDao;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Nando
+ */
+@ManagedBean
+@SessionScoped
+public class LoginController {
+    
+    private Tutor logado;
+    
+    public String logar(String email, String senha) {
+        
+        try{
+            Tutor aux = (Tutor)ManagerDao.getCurrentInstance().read("select t from Tutor t where t.email=\""+email+"\" and t.senha=\""+senha+"\"", Tutor.class).get(0);
+
+            this.logado = aux;
+
+            return "indexTutor.xhtml";
+        
+        } catch(IndexOutOfBoundsException o) {
+            
+                FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao logar",
+                    "As informacoes do login estao incorretas"));
+                
+                return null;
+        }
+    }
+
+    public Tutor getLogado() {
+        return logado;
+    }
+
+    public void setLogado(Tutor logado) {
+        this.logado = logado;
+    }
+    
+    
+    
+    
+}
